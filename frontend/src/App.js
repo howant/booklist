aa7c722ac13c0cba929dc55f5045a1efe2508b8c
Ho-Wan <ho.wan2345@gmail.com>
2018-09-25 15:04:34 +0100

    Hardcoded list of books added to view.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,60 @@
-import React, { Component } from 'react';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <h1 className="App-title">Hello React</h1>
+          <h1 className="App-title">My Book List</h1>
         </header>
+        <div className="App-booklist">
+          <h2>To read</h2>
+          <ul className="Books-to-read">
+            <li>
+              Lead with Respect
+              <ul>
+                <li>Michael Ballé & Freddy Ballé</li>
+              </ul>
+            </li>
+            <li>
+              Book 2
+              <ul>
+                <li>Author 1</li>
+              </ul>
+            </li>
+            <li>
+              Book 3
+              <ul>
+                <li>Author 3</li>
+              </ul>
+            </li>
+          </ul>
+          <h2>Currently reading</h2>
+          <ul className="Books-reading">
+            <li>
+              Book 1
+              <ul>
+                <li>Author 1</li>
+              </ul>
+            </li>
+          </ul>
+          <h2>Finished reading</h2>
+          <ul className="Books-finished">
+            <li>
+              Book 1
+              <ul>
+                <li>Author 1</li>
+              </ul>
+            </li>
+            <li>
+              Book 2
+              <ul>
+                <li>Author 2</li>
+              </ul>
+            </li>
+          </ul>
+        </div>
       </div>
     );
   }
