9cbba3a7ab19594cc3b72bb1ab03e22afc5c177e
Ho-Wan <ho.wan2345@gmail.com>
2018-09-26 10:47:34 +0100

    Booklist changed to react component, proptypes amended.

--- frontend/src/components/Booklist.js
+++ frontend/src/components/Booklist.js
@@ -1,28 +1,33 @@
-import React from "react";
+import React, { Component } from "react";
 import PropTypes from "prop-types";
 
-const Booklist = props => {
-  return (
-    <div className="App-booklist">
-      <h2>To read</h2>
-      <ul className="Books-to-read">
-        {props.lists["To-read"].map(book => (
-          <Book key={book.Title} Title={book.Title} Author={book.Author} />
-        ))}
-      </ul>
-      <h2>Currently reading</h2>
-      <ul className="Books-reading">
-        <Book Title={props.lists["Reading"].Title} Author={props.lists["Reading"].Author} />
-      </ul>
-      <h2>Finished reading</h2>
-      <ul className="Books-finished">
-        {props.lists["Finished-reading"].map(book => (
-          <Book key={book.Title} Title={book.Title} Author={book.Author} />
-        ))}
-      </ul>
-    </div>
-  );
-};
+class Booklist extends Component {
+  render() {
+    return (
+      <div className="App-booklist">
+        <h2>To read</h2>
+        <ul className="Books-to-read">
+          {this.props.lists["To-read"].map(book => (
+            <Book key={book.Title} Title={book.Title} Author={book.Author} />
+          ))}
+        </ul>
+        <h2>Currently reading</h2>
+        <ul className="Books-reading">
+          <Book
+            Title={this.props.lists["Reading"].Title}
+            Author={this.props.lists["Reading"].Author}
+          />
+        </ul>
+        <h2>Finished reading</h2>
+        <ul className="Books-finished">
+          {this.props.lists["Finished-reading"].map(book => (
+            <Book key={book.Title} Title={book.Title} Author={book.Author} />
+          ))}
+        </ul>
+      </div>
+    );
+  }
+}
 
 const Book = props => {
   return (
@@ -37,11 +42,11 @@ const Book = props => {
 
 Booklist.propTypes = {
   book: PropTypes.array
-}
+};
 
 Book.propTypes = {
   Title: PropTypes.string,
   Author: PropTypes.string
-}
+};
 
 export default Booklist;
