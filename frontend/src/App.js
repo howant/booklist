69f9273c4f4211d47fce3703fccc5015424d0707
howant <howant@theodo.co.uk>
2018-09-27 11:24:04 +0100

    jest test added for Booklist component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
 import Booklist from "./containers/Booklist";
-import "./App.css";
+// import "./App.css";
 
 class App extends Component {
   render() {
--- frontend/src/App.test.js
+++ frontend/src/App.test.js
@@ -1,9 +1,48 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
+// import ReactDOM from 'react-dom';
+// import App from './App';
+import Booklist from './components/Booklist';
+import renderer from 'react-test-renderer';
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
+// it('renders without crashing', () => {
+//   const div = document.createElement('div');
+//   ReactDOM.render(<App />, div);
+//   ReactDOM.unmountComponentAtNode(div);
+// });
+
+it('renders correctly', () => {
+  const lists = {
+    "To-read": [
+      {
+        "Title": "Lead With Respect",
+        "Author": "Michael Ballé & Freddy Ballé"
+      },
+      {
+        "Title": "Star Shroud",
+        "Author": "Ken Lozito"
+      },
+      {
+        "Title": "The Great Gatsby",
+        "Author": "F. Scott Fitzgerald"
+      }
+    ],
+    "Reading": {
+      "Title": "Black Man",
+      "Author": "Richard Morgan"
+    },
+    "Finished-reading": [
+      {
+        "Title": "Dark Eden",
+        "Author": "Chris Beckett"
+      },
+      {
+        "Title": "A Wild Sheep Chase",
+        "Author": "Haruki Murakami"
+      }
+    ]
+  };
+  const tree = renderer
+    .create(<Booklist lists={lists} requestBooks={jest.fn()}/>)
+    .toJSON();
+  expect(tree).toMatchSnapshot();
 });
