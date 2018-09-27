87264a372a728ab96bffe39f3d2077eb32b35fe6
howant <howant@theodo.co.uk>
2018-09-27 15:58:42 +0100

    jest snapshot nested selectedBook fixed

--- frontend/src/tests/Description.test.js
+++ frontend/src/tests/Description.test.js
@@ -1,16 +1,12 @@
-import React from 'react';
-import Description from '../components/Description';
-import renderer from 'react-test-renderer';
+import React from "react";
+import Description from "../components/Description";
+import renderer from "react-test-renderer";
 
-it('renders correctly', () => {
-  const lists = {
-    selectedBook: {
-      title: "Book One",
-      synopsis: "synopsis goes here..."
-    }
-  }
-  const tree = renderer
-    .create(<Description selectedBook={lists}/>)
-    .toJSON();
+it("renders correctly", () => {
+  const selectedBook = {
+    title: "Book One",
+    synopsis: "synopsis goes here..."
+  };
+  const tree = renderer.create(<Description selectedBook={selectedBook} />).toJSON();
   expect(tree).toMatchSnapshot();
 });
