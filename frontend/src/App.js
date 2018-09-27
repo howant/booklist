8d47c91c54bd5de34aaaaa2e37acb7f8d0713e7d
howant <howant@theodo.co.uk>
2018-09-27 12:46:07 +0100

    /description route added using react-router-dom

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,16 +1,22 @@
 import React, { Component } from "react";
 import Booklist from "./containers/Booklist";
-// import "./App.css";
+import Description from "./components/Description";
+
+import "./App.css";
+import { BrowserRouter as Router, Route } from "react-router-dom";
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <h1 className="App-title">My Book List</h1>
-        </header>
-        <Booklist/>
-      </div>
+      <Router>
+        <div className="App">
+          <header className="App-header">
+            <h1 className="App-title">My Book List</h1>
+          </header>
+          <Route exact path="/" component={Booklist} />
+          <Route path="/description" component={Description} />
+        </div>
+      </Router>
     );
   }
 }
