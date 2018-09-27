c9e84a4031a44bf62b6adb107576aa23dd9811a8
howant <howant@theodo.co.uk>
2018-09-27 13:09:56 +0100

    jest tests moved to test folder. Description test added.

--- frontend/src/tests/Booklist.test.js
+++ frontend/src/tests/Booklist.test.js
@@ -1,14 +1,6 @@
 import React from 'react';
-// import ReactDOM from 'react-dom';
-// import App from './App';
-import Booklist from './components/Booklist';
+import Booklist from '../components/Booklist';
 import renderer from 'react-test-renderer';
-
-// it('renders without crashing', () => {
-//   const div = document.createElement('div');
-//   ReactDOM.render(<App />, div);
-//   ReactDOM.unmountComponentAtNode(div);
-// });
 
 it('renders correctly', () => {
   const lists = {
---
+++ frontend/src/tests/Description.test.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import Description from '../components/Description';
+import renderer from 'react-test-renderer';
+
+it('renders correctly', () => {
+
+  const tree = renderer
+    .create(<Description/>)
+    .toJSON();
+  expect(tree).toMatchSnapshot();
+});
